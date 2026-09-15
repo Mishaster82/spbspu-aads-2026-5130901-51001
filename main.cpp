78df43402fdc51bcd60c589d1c5660e201b7ee88
Mishaster82 <misha.novikov2007@gmail.com>
2026-09-15 23:02:12 +0300

    final version

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
   try {
     novikov::CommandProcessor processor(std::cin, std::cout, std::cerr);
     return processor.run();
-  } catch (const std::exception &error) {
+  } catch (const std::exception& error) {
     std::cerr << "Internal error: " << error.what() << '\n';
     return 2;
   }
